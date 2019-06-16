8d79571104dcf86447658df8015a9bccab9ba690
AmirthaPappuraj <51657023+AmirthaPappuraj@users.noreply.github.com>
2019-06-16 14:40:49 +0530

    Update AreaRound5.py

--- CodeKata/AreaRound5.py
+++ CodeKata/AreaRound5.py
@@ -1,11 +1,10 @@
 import decimal
+def roundoff(n):
+  digit=5
+  temp=str(decimal.Decimal(str(n)+'0' * digit))
+  return temp[:temp.find('.')+digit+1]
 
-def roundoff(x):
-  d=5
-  temp=str(decimal.Decimal(str(x)+'0' * d))
-  return temp[:temp.find('.')+d+1]
-
-num1,num2=map(float,input().split())
-area=num1*num2
-output=roundoff(area)
-print(output)
+n1,n2=map(float,input().split())
+a=n1*n2
+result=roundoff(a)
+print(result)
