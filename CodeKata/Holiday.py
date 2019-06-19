a72d2f5881a7533628897f35bfd6e8362907286a
AmirthaPappuraj <51657023+AmirthaPappuraj@users.noreply.github.com>
2019-06-19 22:17:03 +0530

    Create Holiday.py

---
+++ CodeKata/Holiday.py
@@ -0,0 +1,17 @@
+day=str(input())
+if(day=='Sunday'):
+  print('yes')
+elif(day=='Monday'):
+  print('no')
+elif(day=='Tuesday'):
+  print('no')
+elif(day=='Wednesday'):
+  print('no')
+elif(day=='Thursday'):
+  print('no')
+elif(day=='Friday'):
+  print('no')
+elif(day=='Saturday'):
+  print('no')
+else:
+  exit()
