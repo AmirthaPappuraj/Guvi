9fbaec05f9f07726dac3b41caf4ae911951f6448
AmirthaPappuraj <51657023+AmirthaPappuraj@users.noreply.github.com>
2019-07-17 23:23:34 +0530

    Update Player86.py

--- CodeKata/Player86.py
+++ CodeKata/Player86.py
@@ -1,9 +1,8 @@
 n=int(input())
-if(n==2):
-  a,b=map(int,input().split())
-  c=a^b
-  print(c)
-elif(n==3):
-  a,b,c=map(int,input().split())
-  d=a^b^c
-  print(d)
+a=[int(x) for x in input().split()]
+b=0
+if n==len(a):
+    for i in range(0,n):
+        for j in range(i+1,n):
+            b=a[i]^a[j]
+print(b)
